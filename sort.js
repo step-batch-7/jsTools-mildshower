d7299ae68f577b504c9edbca0c702e3dfb4013c5
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2019-12-31 16:22:52 +0530

    refactored sort.js for for optimizing early exit

--- sort.js
+++ sort.js
@@ -3,12 +3,12 @@ const {stdout, stderr} = process;
 const performSort = require('./src/performSort');
 
 const showOutput = function(sortOutput) {
-  if (sortOutput.errorMsg) {
-    stderr.write(sortOutput.errorMsg + '\n');
-    process.exitCode = 2;
+  if (sortOutput.sortedContent !== undefined) {
+    stdout.write(sortOutput.sortedContent + '\n');
     return;
   } 
-  stdout.write(sortOutput.sortedContent + '\n');
+  stderr.write(sortOutput.errorMsg + '\n');
+  process.exitCode = 2;
 };
 
 const main = function() {
